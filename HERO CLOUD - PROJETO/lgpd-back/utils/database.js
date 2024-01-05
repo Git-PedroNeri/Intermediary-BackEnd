9c74afe36d81450f9d4bc5f020a01f9b77ed593b
Git-PedroNeri <neri.pedro@icloud.com>
2024-01-04 22:32:20 -0300

    update

--- HERO CLOUD - PROJETO/lgpd-back/utils/database.js
+++ HERO CLOUD - PROJETO/lgpd-back/utils/database.js
@@ -1,17 +1,12 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize (
-    'lgpd-database',
-    'postgres',
-    'postgres',
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-        define: {
-            timestamps: false
-        }
-    }
-);
+const sequelize = new Sequelize('lgpd-database', 'postgres', 'root', {
+  host: 'localhost',
+  dialect: 'postgres',
+  port: 5432,
+  define: {
+    timestamps: false,
+  },
+});
 
 export default sequelize;
--- Hello NPM/demo-fs/demo-fs.js
+++ Hello NPM/demo-fs/demo-fs.js
@@ -2,8 +2,8 @@ import { createServer } from 'http';
 import { readFile } from 'fs';
 
 createServer(function (req, res) {
-  readFile('demo-readfile.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  readFile('demo-readfile.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
     return res.end();
   });
